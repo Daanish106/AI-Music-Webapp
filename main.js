edc88eb7288ebfbec133d52ce1f9d42350dd52c1
Daanish106 <76904703+Daanish106@users.noreply.github.com>
2022-07-22 22:16:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,27 +6,33 @@ rightWristX = 0;
 leftWristY = 0;
 rightWristY = 0;
 
-function preload()
-{
-    song_H = loadSound("Harry Potter Theme.mp3");
-    song_P = loadSound("pirats of the caribin.mp3");
+function preload() 
+{  
+   song1 = loadSound("Harry Potter Theme.mp3"); 
+   song2 = loadSound("pirates of the caribin.mp3"); 
 }
 
 function setup()
 {
-    canvas.createCanvas(600, 500);
+    canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    PoseNet.on('pose', gotPoses)
+    poseNet = ml5.poseNet(video, modelLoaded());
+    poseNet.on('pose', gotPoses);
    
 }
+
+function modelLoaded()
+{
+    console.log("Model Loaded")
+}
+
 function draw()
 {
-    image(video, 0, 0, 600, 500)
+    image(video, 0, 0, 600, 500);
 }
 
 function gotPoses(results)
